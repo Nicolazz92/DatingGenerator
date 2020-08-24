eb88cff365c2c6b7074ccf04e016111c262ae071
Nicolazz92 <gurzufina@yandex.ru>
2020-08-24 18:29:10 +0300

    init

---
+++ src/main/java/com/velikokhatko/DatingGenerator.java
@@ -0,0 +1,13 @@
+package com.velikokhatko;
+
+/**
+ * Hello world!
+ *
+ */
+public class DatingGenerator
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
